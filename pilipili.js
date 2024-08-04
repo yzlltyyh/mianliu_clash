be394f522e4a0071ff44438b0f21ab1c050938ab
yzlltyyh <109973085+yzlltyyh@users.noreply.github.com>
2024-08-04 21:23:36 +0800

    delete js

--- pilipili.js
+++
@@ -1,9 +0,0 @@
-var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
-(function(){
-var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
-s1.async=true;
-s1.src='https://embed.tawk.to/667636baeaf3bd8d4d13348d/1i0usbi93';
-s1.charset='UTF-8';
-s1.setAttribute('crossorigin','*');
-s0.parentNode.insertBefore(s1,s0);
-})();
